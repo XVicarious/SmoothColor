23721e5951bed3a837e8c9338e476ce7b6825a57
Brian Maurer <bmaurer@xvicario.us>
2013-03-10 21:44:40 -0400

    updated

--- buildSC.sh
+++ buildSC.sh
@@ -3,11 +3,23 @@
 if [ $# -lt 1 ]
 then
   echo "Usage: $0 color"
-  echo "Where color is capital"
+  echo "Where color is {red,blue}"
 fi
 
 case "$1" in
-Red)  echo "@import \"SmoothRed-Colors\"" > tmpInclude
-      echo "@import \"SmoothRed-Images\"" >> tmpInclude
-      cat SmoothColor-Includes.less >> tmpInclude
+red)  rm SmoothColor-Includes.less
+      echo "@import \"SmoothRed-Colors\";" > tmpInclude
+      echo "@import \"SmoothRed-Images\";" >> tmpInclude
       mv tmpInclude SmoothColor-Includes.less
+      echo "Generated Include File..."
+      lessc SmoothColor.less SmoothRed.css
+      echo "Compile Completed!"
+      ;;
+blue) rm SmoothColor-Includes.less
+      echo "@import \"SmoothBlue-Colors\";" > tmpInclude
+      mv tmpInclude SmoothColor-Includes.less
+      echo "Unable to build at this time"
+      ;;
+*)    echo "Unknown color $1"
+      ;;
+esac
